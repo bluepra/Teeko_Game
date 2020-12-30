8cbaeb29b0e7ad9a6e31352777d7493712785be8
Ryan Almizyed <ryanbrahim@aol.com>
2020-12-30 03:26:36 -0600

    Implemented move_phase

--- game.py
+++ game.py
@@ -82,7 +82,6 @@ class Board:
     cells = []
 
     def __init__(self):
-        # print('creating board')
         for row in range(5):
             row_of_cells = []
             for col in range(5):
@@ -98,6 +97,18 @@ class Board:
             for col in range(5):
                 curr_cell = self.cells[row][col]
                 curr_cell.draw(surface)
+
+    def reset(self):
+        temp_cells = []
+        for row in range(5):
+            row_of_cells = []
+            for col in range(5):
+                x = Board.PADDING + (Cell.CELL_LENGTH + Board.PADDING) * col
+                y = Board.PADDING + (Cell.CELL_LENGTH + Board.PADDING) * row
+                new_cell = Cell(x, y, (row, col))
+                row_of_cells.append(new_cell)
+            temp_cells.append(row_of_cells)
+        self.cells = temp_cells
 
 
 class Button:
@@ -124,14 +135,6 @@ class Button:
         else:
             self.font = myfont
 
-    def clicked(self):
-        if(self.text == 'START'):
-            run_game()
-        if(self.text == 'TUTORIAL'):
-            run_tutorial()
-        if(self.text == 'EXIT'):
-            sys.exit()
-
 
 class TextBar:
     def __init__(self, text, x, y, width, height, color):
@@ -151,7 +154,7 @@ class TextBar:
     def update_text(self, new_text):
         self.text = new_text
 
-
+# Create the Board
 board = Board()
 text_bar = TextBar('click on a square', Board.PADDING,
                    SCREEN_WIDTH - Board.PADDING, SCREEN_WIDTH / 2, 45, BLACK)
@@ -170,10 +173,7 @@ def place_piece(cell_coord, color):
     board.cells[y][x].put_piece(color)
 
 
-def move_piece(source, desti):
-    # Get the cells from the board
-    sourceCell = board.cells[source[0]][source[1]]
-    destiCell = board.cells[desti[0]][desti[1]]
+def move_piece(sourceCell, destiCell):
     # Checking if there is a piece to move
     if(not sourceCell.has_piece()):
         return False
@@ -181,16 +181,20 @@ def move_piece(source, desti):
     if(destiCell.has_piece()):
         return False
     # Checking if the two cells are adjacent
-    if(not isAdjacent(source, desti)):
+    if(not isAdjacent(sourceCell, destiCell)):
         return False
-    # No erros, moving piece
-    sourceCell.put_piece()
-    destiCell.put_piece()
+    # Save sourceCell's old color
+    tempColor = sourceCell.color
+    # No errors, moving piece
+    sourceCell.put_piece(sourceCell.color)
+    destiCell.put_piece(tempColor)
     # Succesfully moved the piece
     return True
 
 
-def isAdjacent(pos1, pos2):
+def isAdjacent(cell1, cell2):
+    pos1 = cell1.cell_coord
+    pos2 = cell2.cell_coord
     return (abs(pos1[0] - pos2[0]) <= 1) and (abs(pos1[1] - pos2[1]) <= 1)
 
 
@@ -202,38 +206,8 @@ def check_buttons(buttons, click_pos):
             else:
                 continue
 
-# Run Game
 
-
-def run_game():
-    running = True
-
-    # Create the AI object
-    ai = Teeko2Player()
-
-    drop_phase(ai)
-    move_phase(ai)
-
-    '''while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                # Left click
-                if pygame.mouse.get_pressed()[0]:
-                    # USER TURN
-                    # User places a piece
-                    pos = pygame.mouse.get_pos()
-                    cell_coord = get_cell_coord(pos)
-                    place_piece(cell_coord)
-'''
-
-
-def drop_phase(ai):
-    piece_count = 0
-    # Randomly choose whether user or Ai goes first
-    userTurn = random.choice([True, False])
+def generateColors():
     # Randomly choose user's piece color
     userColor = random.choice([red, black])
     # Choose the AI's color to be opposite of user's color
@@ -242,6 +216,33 @@ def drop_phase(ai):
         AIColor = black
     else:
         AIColor = red
+    colors = (userColor, AIColor)
+    return colors
+
+
+def run_game():
+    # Randomly choose whether user or Ai goes first
+    userTurn = random.choice([True, False])
+    # Randomly generate user & AI piece colors
+    colors = generateColors()
+
+    # Create the AI object
+    ai = Teeko2Player()
+
+    # Run drop phase
+    drop_phase(ai, userTurn, colors)
+    # Run move phase
+    winner = move_phase(ai, userTurn, colors)
+
+    # Display winner
+    win_screen(winner)
+    del ai
+
+
+def drop_phase(ai, userTurn, colors):
+    piece_count = 0
+    userColor = colors[0]
+    AIColor = colors[1]
 
     # Loop until all pieces are placed
     while piece_count < 8:
@@ -269,9 +270,8 @@ def drop_phase(ai):
 def userDropHandler(event, ai, userColor):
     # Check if left click, then place piece
     if pygame.mouse.get_pressed()[0]:
-        pos = pygame.mouse.get_pos()
-        cell_coord = get_cell_coord(pos)
-        cur_cell = board.cells[cell_coord[0]][cell_coord[1]]
+        cur_cell = getMouseCell()
+        cell_coord = cur_cell.cell_coord
         if(cur_cell.has_piece()):
             return False
         place_piece(cell_coord, userColor)
@@ -279,10 +279,87 @@ def userDropHandler(event, ai, userColor):
     return True
 
 
-def move_phase(ai):
+def move_phase(ai, userTurn, colors):
+    userColor = colors[0]
+    AIColor = colors[1]
 
-    # Opening menu
-    return
+    user_source = None
+    user_desti = None
+
+    # Loop while game is still undecided
+    while ai.game_value(ai.board) == 0:
+        # Get all current events
+        for event in pygame.event.get():
+            # Quit event
+            if event.type is pygame.QUIT:
+                quitHandler()
+            # User's turn
+            if(userTurn):
+                # Check if user clicked on a rectangle
+                if checkLeftClick(event):
+                    # Get the cell that has been clicked
+                    cell = getMouseCell()
+                    # If the cell has a piece, select source cell
+                    if(cell.has_piece and cell.color is userColor):
+                        user_source = cell
+                    # If the cell is empty, select a destination cell
+                    if(not cell.has_piece() and user_source is not None):
+                        user_desti = cell
+                # User has selected a source and destination,
+                if user_source is not None and user_desti is not None:
+                    # try to move the piece
+                    if move_piece(user_source, user_desti):
+                        ai.opponent_move(
+                            [user_desti.cell_coord, user_source.cell_coord])
+                        userTurn = False
+                    # failed to move the piece, reset selection
+                    else:
+                        user_source = None
+                        user_desti = None
+            # AI's turn
+            else:
+                ai_move = ai.make_move(ai.board)
+                ai.place_piece(ai_move, ai.my_piece)
+                AISource = getCellFromCoord(ai_move[1])
+                AIDesti = getCellFromCoord(ai_move[0])
+                move_piece(AISource, AIDesti)
+                userTurn = True
+        # Draw board at end of each loop
+        draw_board()
+    if ai.game_value(ai.board) == 1:
+        return "AI wins! Game over."
+    else:
+        return "You win! Game over."
+
+
+def checkLeftClick(event):
+    return event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]
+
+
+def getMouseCellCoord():
+    pos = pygame.mouse.get_pos()
+    return get_cell_coord(pos)
+
+
+def getMouseCell():
+    coord = getMouseCellCoord()
+    return getCellFromCoord(coord)
+
+
+def getCellFromCoord(coord):
+    return board.cells[coord[0]][coord[1]]
+
+
+def win_screen(winner):
+    board.reset()
+
+    # DEBUGGING
+    for row in range(5):
+        rowStr = ""
+        for col in range(5):
+            rowStr += " " + str(board.cells[row][col].color)
+        print(rowStr)
+    print(winner)
 
 
 def draw_board():
@@ -323,7 +400,12 @@ def run_menu():
                     pos = pygame.mouse.get_pos()
                     clicked_button = check_buttons(buttons, pos)
                     if clicked_button is not None:
-                        clicked_button.clicked()
+                        if(clicked_button.text == 'START'):
+                            run_game()
+                        if(clicked_button.text == 'TUTORIAL'):
+                            run_tutorial()
+                        if(clicked_button.text == 'EXIT'):
+                            sys.exit()
 
         menu_surface.blit(hexagon, (12, 12))
 
@@ -340,26 +422,8 @@ def run_menu():
         clock.tick(60)
 
 
-# Main event handler loop
-def handleEvents():
-    for event in pygame.event.get():
-        if event.type is pygame.QUIT:
-            quitHandler()
-        elif event.type is pygame.MOUSEBUTTONDOWN:
-            clickHandler()
-
-# Individual event handlers
-
-
 def quitHandler():
     sys.exit()
-
-
-def clickHandler(event):
-    pos = pygame.mouse.get_pos()
-    clickedButton = check_buttons(buttons, pos)
-    if clickedButton is not None:
-        clickedButton.clicked()
 
 
 run_menu()
